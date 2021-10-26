50ece40094d520dc4aa81bade82720e94909d1fc
Arsen Grigoryan <happysmilik@gmail.com>
2021-10-26 09:25:36 +0300

    change program and README.md

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int balance = 2_000_000_000;
         int transferSum = 500_000_000;
-        int total = balance * transferSum;
+        int total = balance + transferSum;
         System.out.println(total);
     }
 }
